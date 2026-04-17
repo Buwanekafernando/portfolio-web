bb8b3e07c5d1a069fb24373ba4b9da04420be4b4
Buwanekafernando <fernandodeshan0@gmail.com>
2026-04-17 16:51:20 +0530

    added the cetificates and links

--- portfolio/src/data/certificates.js
+++ portfolio/src/data/certificates.js
@@ -1,129 +1,163 @@
 export const certificates = [
   {
     id: 1,
-    title: "Microsoft Azure - Visual Machine, Configure Visual Network",
+    title: "Microsoft Azure - Configure virtual machine availability",
     issuer: "Microsoft Learning",
-    date: "August 2025",
-    image: "https://images.unsplash.com/photo-1614064641938-3bbee52942c7?auto=format&fit=crop&q=80&w=800",
-    link: "#"
+    date: "October 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/PGBAU3N4?sharingId=F7F20AAB6BA10455"
   },
   {
     id: 2,
     title: "AWS - Connecting VPCs",
     issuer: "AWS SimuLean",
     date: "August 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/46NC4HQFUY/aws-simulearn-connecting-vpcs/J7QJJRTCTG"
   },
   {
     id: 3,
     title: "Machine Learning and Advanced AI Techniques",
     issuer: "Alison",
     date: "July 2025",
-    link: "#"
+    link: "https://alison.com/course/machine-learning-and-advanced-ai-techniques"
   },
   {
     id: 4,
     title: "Generative AI and Large Language Model for Beginners",
     issuer: "Alison",
     date: "July 2025",
-    link: "#"
+    link: "https://alison.com/course/generative-ai-and-large-language-models-for-beginners"
   },
   {
     id: 5,
     title: "Data Science Fundamentals - Essential Tools & Concepts",
     issuer: "Alison",
     date: "July 2025",
-    link: "#"
+    link: "https://alison.com/course/data-science-fundamentals-essential-tools-and-concepts"
   },
   {
     id: 6,
     title: "Introduction of Data Science",
     issuer: "Cisco",
     date: "December 2024",
-    link: "#"
+    link: "https://www.netacad.com/courses/introduction-data-science?courseLang=en-US&instance_id=8a5d1881-37ff-4579-b99b-db387452e077"
   },
   {
     id: 8,
     title: "Business Development Essentials",
     issuer: "Alison",
     date: "November 2024",
-    link: "#"
+    link: "https://alison.com/course/business-development-essentials"
   },
   {
     id: 9,
     title: "Diploma in Project Management",
     issuer: "Alison",
     date: "February 2024",
-    link: "#"
+    link: "https://alison.com/course/diploma-in-project-management-revised-2017"
   },
   {
     id: 10,
     title: "AWS - Core Security Concepts",
     issuer: "AWS SimuLean",
     date: "October 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/3KYWQBTSTV/aws-simulearn-core-security-concepts/Q29T2DWZPK"
   },
   {
     id: 11,
     title: "AWS - Databases in Practice",
     issuer: "AWS SimuLean",
     date: "October 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/6VTV7UQ6CY/aws-simulearn-databases-in-practice/3M6ZTC32MX"
   },
   {
     id: 12,
     title: "AWS - Networking Concepts",
     issuer: "AWS SimuLean",
     date: "August 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/XCEMH2NXKJ/aws-simulearn-networking-concepts/QN5P4AJMUV"
   },
   {
     id: 13,
     title: "AWS - File Systems in the Cloud",
     issuer: "AWS SimuLean",
     date: "August 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/A5M8X2CHM3/aws-simulearn-file-systems-in-the-cloud/RAJVAUNMHR"
   },
   {
     id: 14,
     title: "AWS - Introduction to Amazon Simple Storage Service (S3)",
     issuer: "AWS SimuLean",
     date: "September 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/R54NZHEX5K/introduction-to-amazon-simple-storage-service-s3/SKTY8SPYDX"
   },
   {
     id: 15,
     title: "AWS - Computing Solutions",
     issuer: "AWS SimuLean",
     date: "August 2025",
-    link: "#"
+    link: "https://skillbuilder.aws/learn/NZ6D7QYABD/aws-simulearn-computing-solutions/YQA9623D5T"
   },
   {
     id: 16,
     title: "Microsoft Azure - Explore relational database services in Azure",
     issuer: "Microsoft Learning",
-    date: "August 2025",
-    link: "#"
+    date: "September 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/HZFDPSV8?sharingId=F7F20AAB6BA10455"
   },
   {
     id: 17,
     title: "Microsoft Azure - Configure virtual networks",
     issuer: "Microsoft Learning",
     date: "August 2025",
-    link: "#"
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/W2C8USZN?sharingId=F7F20AAB6BA10455"
   },
   {
     id: 18,
     title: "Microsoft Azure - Introduction to Azure virtual machines",
     issuer: "Microsoft Learning",
     date: "August 2025",
-    link: "#"
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/E57TWRRP?sharingId=F7F20AAB6BA10455"
   },
   {
     id: 19,
     title: "Microsoft Azure - Add and size disks in Azure virtual machines",
     issuer: "Microsoft Learning",
     date: "August 2025",
-    link: "#"
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/8Z9QEY2W?sharingId=F7F20AAB6BA10455"
   },
+  {
+    id: 20,
+    title: "Microsoft Azure -Configure Azure Blob Storage",
+    issuer: "Microsoft Learning",
+    date: "September 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/URGS9LT3?sharingId=F7F20AAB6BA10455"
+  },
+  {
+    id: 21,
+    title: "Microsoft Azure - Improve application scalability and resiliency by using Azure Load Balancer",
+    issuer: "Microsoft Learning",
+    date: "October 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/HZNTJA68?sharingId=F7F20AAB6BA10455"
+  },
+  {
+    id: 22,
+    title: "Microsoft Azure - Explore relational database services in Azure",
+    issuer: "Microsoft Learning",
+    date: "October 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/HZFDPSV8?sharingId=F7F20AAB6BA10455"
+  },
+  {
+    id: 23,
+    title: "Microsoft Azure - Describe Azure identity, access, and security",
+    issuer: "Microsoft Learning",
+    date: "October 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/2JPPT98V?sharingId=F7F20AAB6BA10455"
+  },
+  {
+    id: 24,
+    title: "Microsoft Azure - Introduction to Azure HDInsight",
+    issuer: "Microsoft Learning",
+    date: "October 2025",
+    link: "https://learn.microsoft.com/api/achievements/share/en-us/BuwanekaFernando-1205/PGJJFK64?sharingId=F7F20AAB6BA10455"
+  }
 ];
